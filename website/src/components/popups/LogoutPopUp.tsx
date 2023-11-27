ab232239d14dab26019ec0205d02475e4ea64e88
40292229 <mholmes11@qub.ac.uk>
2023-11-27 17:15:55 +0000

    Formatting
    ^^

--- website/src/components/popups/LogoutPopUp.tsx
+++ website/src/components/popups/LogoutPopUp.tsx
@@ -1,23 +1,22 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const LogoutPopUp = ({open, onClose} : any) => {
-    
-    const navigate = useNavigate()
+const LogoutPopUp = ({ open, onClose }: any) => {
+  const navigate = useNavigate()
 
-    const logout = () => {
-        navigate('/')
-    }
+  const logout = () => {
+    navigate('/')
+  }
 
-    if(!open) return null
-    return(
-        <div className="popup">
-            <p>Are you sure you want to log out?</p>
-            <button onClick={logout}>Yes</button>
-            <button onClick={onClose}>No</button>
-        </div>
-    )
-       
+  if (!open) return null
+  return (
+    <div className="popup">
+      <h1>Logout</h1>
+      <p>Are you sure you want to log out?</p>
+      <button onClick={logout}>Yes</button>
+      <button onClick={onClose}>No</button>
+    </div>
+  )
 }
 
 export default LogoutPopUp
