f886954d69aee3bfebedddb0974c348c18272177
40292229 <mholmes11@qub.ac.uk>
2023-12-03 00:56:01 +0000

    Visual Improvements
    Have made the pop ups nicer. Darkened the background when they are open. Will further improve the settings one

--- website/src/components/popups/HelpPopUp.tsx
+++ website/src/components/popups/HelpPopUp.tsx
@@ -4,11 +4,12 @@ const HelpPopUp = ({ open, onClose }: any) => {
   if (!open) return null
   return (
     <div className="popup">
-      <button onClick={onClose}>X</button>
       <h1>Help</h1>
+      <button className="close-btn" onClick={onClose}>X</button>
       <p>To pick a game scenario, click scenario</p>
       <p>To change your settings, click settings</p>
       <p>To logout, click logout</p>
+      <p>To close pop ups, click the x</p>
     </div>
   )
 }
