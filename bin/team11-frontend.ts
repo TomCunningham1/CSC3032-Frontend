e3d4003c415a4ef31d47740c2c77569a63117e1a
Tom Cunningham <tcunningham12@qub.ac.uk>
2024-01-24 19:05:44 +0000

    fix ci

--- bin/team11-frontend.ts
+++ bin/team11-frontend.ts
@@ -4,9 +4,13 @@ import { Team11FrontendStack } from '../lib/team11-frontend-stack'
 import environment from '../lib/config/environment'
 
 const app = new cdk.App()
-new Team11FrontendStack(app, `team11-${environment.environmentName}-frontend-stack`, {
+new Team11FrontendStack(
+  app,
+  `team11-${environment.environmentName}-frontend-stack`,
+  {
     env: {
-        account: '394261647652',
-        region: 'eu-west-1',
-    }
-})
+      account: '394261647652',
+      region: 'eu-west-1',
+    },
+  }
+)
