eca030c5476a48f19408a824170c44997665eddf
Tom Cunningham <tcunningham12@qub.ac.uk>
2024-02-10 21:53:40 +0000

    update URL

--- website/src/config/constants.ts
+++ website/src/config/constants.ts
@@ -1,3 +1,5 @@
-const BACKEND_IP = process.env.REACT_APP_BACKEND_URL
+const BACKEND_IP =
+  `${process.env.REACT_APP_BACKEND_URL}/team11` ||
+  'https://6oxgkmkwh9.execute-api.eu-west-1.amazonaws.com/prod/team11'
 
 export { BACKEND_IP }
