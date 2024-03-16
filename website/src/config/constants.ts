b426c41af45aaeb6cc2707b28b7365a892cfee0f
Tom Cunningham <tcunningham12@qub.ac.uk>
2024-03-16 17:51:00 +0000

    formatting

--- website/src/config/constants.ts
+++ website/src/config/constants.ts
@@ -1,5 +1,4 @@
 const BACKEND_IP = process.env.REACT_APP_BACKEND_URL
-const API_KEY =
-  process.env.REACT_APP_API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY
 
 export { BACKEND_IP, API_KEY }
