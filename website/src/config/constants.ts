94bcb028a38d484962aac5ed0b869069c3952c7f
Tom Cunningham <tcunningham12@qub.ac.uk>
2024-03-16 17:46:34 +0000

    remove default constants

--- website/src/config/constants.ts
+++ website/src/config/constants.ts
@@ -1,4 +1,5 @@
-const BACKEND_IP =
-  process.env.REACT_APP_BACKEND_URL
+const BACKEND_IP = process.env.REACT_APP_BACKEND_URL
+const API_KEY =
+  process.env.REACT_APP_API_KEY
 
-export { BACKEND_IP }
+export { BACKEND_IP, API_KEY }
