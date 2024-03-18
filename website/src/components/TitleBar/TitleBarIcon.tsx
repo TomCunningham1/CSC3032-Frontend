5313ad07c3dee24e4082414a737ccdd4078ec306
Tom Cunningham <tcunningham12@qub.ac.uk>
2024-03-18 16:01:53 +0000

    formatting

--- website/src/components/TitleBar/TitleBarIcon.tsx
+++ website/src/components/TitleBar/TitleBarIcon.tsx
@@ -1,11 +1,7 @@
 import './TitleBar.css'
 
 const TitleBarIcon = () => {
-  return (
-    <div
-      className="TitleBarIcon"
-    />
-  )
+  return <div className="TitleBarIcon" />
 }
 
 export default TitleBarIcon
